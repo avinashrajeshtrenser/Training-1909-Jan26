c6fa74078946845fc4f47d5375ee9630d5b176d9
Avinash Rajesh <1909@trenser.com>
2026-02-23 00:47:14 +0530

    Changed the User constructor and initialized the status

--- Trenser.WarehouseManagementSystem/Trenser.WarehouseManagementSystem/Trenser.WarehouseManagementSystem/User.h
+++ Trenser.WarehouseManagementSystem/Trenser.WarehouseManagementSystem/Trenser.WarehouseManagementSystem/User.h
@@ -10,7 +10,7 @@ private:
 	std::string m_userStatus;
 public:
 	User() : m_userId{ 0 }, m_userName{ "" }, m_password{ "" }, m_role{ "" }, m_userStatus{"Signed Out"} {}
-	User(int userId, std::string userName, std::string password, std::string role) : m_userId{ userId }, m_userName{ userName }, m_password{ password }, m_role{ role } {}
+	User(int userId, std::string userName, std::string password, std::string role) : m_userId{ userId }, m_userName{ userName }, m_password{ password }, m_role{ role }, m_userStatus{ "Active" }{}
 	std::string getRole();
 	std::string getUserName();
 	std::string getPassword();
